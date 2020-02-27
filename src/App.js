7d8049e12d06c6db1803ab949cefbf4957187b96
bavarianrhino <rjriesenberger@gmail.com>
2020-02-27 08:51:05 -0500

    added meta tags and images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import livinglaniersunset from './images/livinglaniersunset.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (        	
+        <div className="main-area center-text" style={{ opacity: '.8', backgroundImage: `url(${livinglaniersunset})` }}>
+            <div className="display-table">
+                <div className="display-table-cell">
+
+                    <h2 className="title font-white"><b>Comming Soon</b></h2>
+                    <h1 className="title font-white"><b>LIVING LANIER</b></h1>
+                    <p className="desc font-white">Lanier Life Group</p>
+
+                    {/* <!-- social-btn --> */}
+                    <ul className="social-btn font-white">
+                        <li><a href="#">facebook</a></li>
+                        <li><a href="#">twitter</a></li>
+                        <li><a href="#">google</a></li>
+                        <li><a href="#">instagram</a></li>
+                    </ul>
+
+                {/* </div> <!-- display-table --> */}
+                </div>
+            {/* </div> <!-- display-table-cell --> */}
+            </div>
+        {/* </div> <!-- main-area --> */}
+        </div>
+    );
 }
 
 export default App;
