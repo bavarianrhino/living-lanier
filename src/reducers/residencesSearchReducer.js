83c1289034cf7ad95b377e1652b2d92476d36c79
bavarianrhino <rjriesenberger@gmail.com>
2020-03-04 15:30:14 -0500

    building out residence reducer for state manipulation

--- src/reducers/residencesSearchReducer.js
+++ src/reducers/residencesSearchReducer.js
@@ -2,12 +2,23 @@ import { FETCH_RESIDENCES } from '../actions/types';
 
 export default function (
     state = {
-        
+        loading: false,
+        last_residence_fetch_query: '',
+        next_residence_fetch_query: '',
+        residence_fetch_count: 0,
+        residence_listings: [],
+
     },action) {
     switch(action.type) {
 
         case FETCH_RESIDENCES:
-            return action.payload
+            return { 
+                ...state,
+                last_residence_fetch_query: action.payload['@odata.context'],
+                next_residence_fetch_query: action.payload['@odata.nextLink'],
+                residence_fetch_count: action.payload['@odata.count'],
+                residence_listings: action.payload.value
+            };
 
         default:
             return state;
