396d997ea511782b1e2e34d648bcab8f85dbef97
bavarianrhino <rjriesenberger@gmail.com>
2020-03-05 02:39:00 -0500

    working reducer and query for fetched landlots

--- src/reducers/propertiesSearchReducer.js
+++ src/reducers/propertiesSearchReducer.js
@@ -1,9 +1,39 @@
 import { FETCH_PROPERTIES } from '../actions/types';
 
-export default function(state = [], action) {
+export default function (
+    state = {
+        loading: false,
+        lastQuery: '',
+        nextQuery: '',
+        lotCount: 0,
+        lotListingsByID: {},
+
+    },action) {
     switch(action.type) {
+
         case FETCH_PROPERTIES:
-            return action.payload
+            const lots = action.payload.value.map((obj, i) => ({ [obj.ListingId]: Object.assign({}, {
+                    cardData: {
+                        coords: [obj.Longitude, obj.Latitude],
+                        daysOnMarket: obj.DaysOnMarket, 
+                        propertyType: obj.PropertyType,
+                        bioText: obj.PublicRemarks,
+                        media: obj.media,
+                        mediaURL: obj.VirtualTourURLUnbranded
+                    }
+                }, {
+                    fullData: Object.assign(obj, Object.entries(obj).forEach( key_val => (key_val[1] === null || key_val[1] == `${Array(0)}` ? delete obj[key_val[0]] : 0)))
+                }
+            )}))
+            console.log(lots)
+            return { 
+                ...state,
+                lastQuery: action.payload['@odata.context'],
+                nextQuery: action.payload['@odata.nextLink'],
+                lotCount: action.payload['@odata.count'],
+                lotListingsByID: lots
+            };
+
         default:
             return state;
     }
