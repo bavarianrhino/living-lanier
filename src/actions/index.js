26e96f653aa1fe69dacc0014b22008a574443d6b
bavarianrhino <rjriesenberger@gmail.com>
2020-03-27 08:12:36 -0400

    added npm and env files

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,17 +12,20 @@ import { FETCH_RESIDENCES, FETCH_PROPERTIES } from './types';
 
 // const keys = require('../utilities/config/keys');
 // const bridgeAPIToken = keys.bridgeInteractiveAPIServerToken;
-// const dataset = keys.MLS_DATASET;
+const dataset = 'fmls';
+const bridge = '80ad3a1a125705';
+const inter = '3fe480a538a0e440e4';
+
 
 // PREVIOUS REFACTORS IN NOTES
 export const fetchResidences = () => async (dispatch) => {
-    const res = await axios.get(`https://api.bridgedataoutput.com/api/v2/OData/${fmls}/Properties?access_token=${BRIDGE_INTERACTIVE_API_SERVER_TOKEN}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(FMLS_MlsPropertyType,%20%27Residential%20Detached%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)`);
-                                // 'https://api.bridgedataoutput.com/api/v2/OData/${fmls}/Properties?access_token=${BRIDGE_INTERACTIVE_API_SERVER_TOKEN}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(FMLS_MlsPropertyType,%20%27Residential%20Detached%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)&$skip=200'
+    const res = await axios.get(`https://api.bridgedataoutput.com/api/v2/OData/${dataset}/Properties?access_token=${bridge + inter}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(FMLS_MlsPropertyType,%20%27Residential%20Detached%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)`);
+                                // 'https://api.bridgedataoutput.com/api/v2/OData/${dataset}/Properties?access_token=${BRIDGE_INTERACTIVE_API_SERVER_TOKEN}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(FMLS_MlsPropertyType,%20%27Residential%20Detached%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)&$skip=200'
     dispatch({ type: FETCH_RESIDENCES, payload: res.data });
 };
 
 export const fetchProperties = () => async (dispatch) => {
-    const res = await axios.get(`https://api.bridgedataoutput.com/api/v2/OData/${fmls}/Properties?access_token=${BRIDGE_INTERACTIVE_API_SERVER_TOKEN}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(PropertyType,%20%27Land%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)`);
+    const res = await axios.get(`https://api.bridgedataoutput.com/api/v2/OData/${dataset}/Properties?access_token=${bridge + inter}&$top=200&$orderby=DaysOnMarket&$filter=contains(WaterBodyName,%20%27Lanier%27)%20and%20year(ListingContractDate)%20gt%202018%20and%20contains(FMLS_PreviousStatus,%20%27Active%27)%20and%20contains(PropertyType,%20%27Land%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27None%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Courtesy%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Shared%27)%20and%20FMLS_Dock/any(a:%20a%20ne%20%27Other%27)`);
     
     dispatch({ type: FETCH_PROPERTIES, payload: res.data });
 };
