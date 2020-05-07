cf189232cd7e1da883b6753a9209dabc51b3631e
bavarianrhino <rjriesenberger@gmail.com>
2020-05-07 09:07:35 -0400

    react app env vars console logged

--- src/index.js
+++ src/index.js
@@ -30,7 +30,11 @@ console.log(`BRIDGE INTERACTIVE API KEY ---- ${process.env.BRIDGE_INTERACTIVE_AP
 console.log(`GMAPS API KEY ---- ${process.env.GOOGLE_MAPS_API_KEY}`) //process.env.REACT_APP_KEY
 console.log(`TEST API KEY ---- ${process.env.TEST_API_KEY}`) //process.env.REACT_APP_KEY
 console.log(`GREETING ---- ${process.env.GREETING}`) //process.env.REACT_APP_KEY
-console.log(`GREETING ---- ${process.env.REACT_APP_API_TEST_KEY}`) //process.env.REACT_APP_KEY
+console.log(`REACT APP ---- ${process.env.REACT_APP_API_TEST_KEY}`) //process.env.REACT_APP_KEY
+console.log(`REACT BRIDGE INTERACTIVE API KEY ---- ${process.env.REACT_APP_API_BRIDGE_INTERACTIVE_API_SERVER_TOKEN}`) //process.env.REACT_APP_KEY
+console.log(`REACT GMAPS API KEY ---- ${process.env.REACT_APP_API_GOOGLE_MAPS_API_KEY}`) //process.env.REACT_APP_KEY
+console.log(`REACT GREETING ---- ${process.env.REACT_APP_GREETING}`) //process.env.REACT_APP_KEY
+console.log(`TEST API KEY ---- ${process.env.REACT_APP_TEST_API_KEY}`) //process.env.REACT_APP_KEY
 // console.log(`GREETING ---- ${GREETING}`) //process.env.REACT_APP_KEY
 // console.log(GREETING) //process.env.REACT_APP_KEY
 serviceWorker.unregister();
